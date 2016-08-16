a7e5f032a580e70c8bb3e9ac5e324983de808464
Wood, Christopher <CJW@stowers.org>
2016-08-16 10:35:23 -0500

    Added HistogramPlot

--- src/main/java/org/stowers/microscopy/utils/Histogram.java
+++ src/main/java/org/stowers/microscopy/utils/Histogram.java
@@ -1,11 +1,14 @@
 package org.stowers.microscopy.utils;
 
+import java.util.List;
+import java.util.ArrayList;
+
 /**
  * Created by cjw on 7/27/16.
  */
 public class Histogram {
 
-    int[] counts;
+    double[] counts;
     double[] bins;
     double[] data;
 
@@ -14,18 +17,19 @@ public class Histogram {
     double dataMax;
     double startx;
     double endx;
+    double hmax;
 
     public Histogram(double[] data, int xnbins) {
         this.data = data;
         this.nbins = xnbins;
         findDataMinMax();
         makeBins();
-        counts = new int[nbins];
+        counts = new double[nbins];
         System.out.println(dataMin + " " + dataMax);
         doHist();
     }
 
-    protected int[] doHist() {
+    public double[] doHist() {
 
         for (int i = 0; i < data.length; i++) {
 
@@ -40,9 +44,57 @@ public class Histogram {
 //            System.out.println(x + " " + k);
         }
 
+        for (int i = 0; i < counts.length; i++) {
+            if (counts[i] > hmax) {
+                hmax = counts[i];
+            }
+        }
         return counts;
     }
 
+    public void setBins(double[] cbins) {
+        bins = cbins;
+        nbins = bins.length;
+    }
+
+    public void setStartX(double x) {
+        startx = x;
+    }
+
+    public void setEndX(double x) {
+        endx = x;
+    }
+
+    public double[][] getHistogramArray(boolean logY) {
+
+        double[][] res =  new double[2][counts.length];
+
+        if (!logY) {
+            res[1] = counts;
+            for (int i = 1; i <= counts.length; i++) {
+                res[0][i - 1] = (bins[i - 1] + bins[i]) / 2.;
+            }
+        }
+        else {
+            List<double[]> c = new ArrayList<>();
+            for (int i = 0; i < counts.length; i++) {
+                if (counts[i] > 0) {
+                    double b =  (bins[i] + bins[i + 1]) / 2.;
+                    c.add(new double[] {b, counts[i]});
+                }
+            }
+            for (int i = 0; i < c.size(); i++) {
+                double[] temp = c.get(i);
+                res[0][i] = temp[0];
+                res[1][i] = temp[1];
+            }
+        }
+        return res;
+    }
+
+    public double getHmax() {
+        return hmax;
+    }
     public double[] getBins() {
         return bins;
     }
---
+++ src/main/java/org/stowers/microscopy/utils/HistogramPlot.java
@@ -0,0 +1,113 @@
+package org.stowers.microscopy.utils;
+
+/**
+ * Created by cjw on 7/27/16.
+ */
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.AxisLocation;
+import org.jfree.chart.axis.LogAxis;
+import org.jfree.chart.axis.LogarithmicAxis;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.renderer.xy.StandardXYBarPainter;
+import org.jfree.chart.renderer.xy.XYBarRenderer;
+import org.jfree.data.xy.DefaultXYDataset;
+import org.jfree.data.xy.XYBarDataset;
+
+import java.awt.*;
+
+public class HistogramPlot {
+
+    JFrame frame;
+    ChartPanel chartpanel;
+    JFreeChart chart;
+
+    Histogram h;
+
+    boolean logY;
+
+    public HistogramPlot(Histogram h) {
+        this.h = h;
+    }
+
+    public HistogramPlot(double[] d, int nbins) {
+        Histogram  h = new Histogram(d, nbins);
+    }
+
+    public void plotHist() {
+        frame = new JFrame("Histogram");
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        makeChart("Title", "Data", "Frequency");
+
+        frame.add(chartpanel);
+        frame.setSize(800, 600);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public ChartPanel makeChart(String title, String xlabel, String ylabel) {
+
+        DefaultXYDataset dataset = new DefaultXYDataset();
+        double[][] hdata = h.getHistogramArray(logY);
+
+        dataset.addSeries("Hist", hdata);
+        double barWidth = 0.8*(h.getEndX() - h.getStartX())/h.getNBins();
+        XYBarDataset xybar = new XYBarDataset(dataset, barWidth);
+
+        chart = ChartFactory.createXYBarChart(title, xlabel, false, ylabel, xybar);
+
+        XYPlot plot = (XYPlot)chart.getPlot();
+
+        XYBarRenderer renderer = (XYBarRenderer) plot.getRenderer();
+        renderer.setBarPainter(new StandardXYBarPainter());
+        renderer.setSeriesPaint(0, Color.orange);
+
+        if (logY) {
+            LogAxis log = new LogAxis("log(Counts)");
+            log.setRange(.1, h.getHmax());
+            plot.setRangeAxis(log);
+        }
+        plot.setBackgroundPaint(Color.white);
+        plot.setDomainGridlinePaint(Color.lightGray);
+        plot.setRangeGridlinePaint(Color.lightGray);
+        plot.setOutlineVisible(false);
+        plot.setDomainAxisLocation(AxisLocation.BOTTOM_OR_LEFT);
+        plot.setRangeAxisLocation(AxisLocation.BOTTOM_OR_LEFT);
+
+        chartpanel = new ChartPanel(chart);
+
+
+        return chartpanel;
+
+    }
+
+    public void setLogY(boolean logY) {
+        this.logY = logY;
+    }
+
+    public static void main(String[] args) {
+
+        double[] x = new double[100];
+        java.util.Random rnd = new java.util.Random();
+        for (int i = 0; i < x.length; i++) {
+//            x[i] = rnd.nextGaussian();
+            x[i] = -Math.log(rnd.nextDouble());
+        }
+
+        Histogram h = new Histogram(x, 10);
+        HistogramPlot p = new HistogramPlot(h);
+//        p.setLogY(true);
+//        p.plotHist();
+        HistogramPlot p1 = new HistogramPlot(h);
+        p1.setLogY(false);
+        p1.plotHist();
+
+    }
+
+}
